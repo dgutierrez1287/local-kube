4a6427b4ec4a7bd8c2c8b4e5291a52fdfc8d3b45
Diego Gutierrez <dgutierrez1287@gmail.com>
2025-03-23 19:01:30 -0400

    changed logger initialization to make testing easier and have it as part of the logging package

--- cmd/root.go
+++ cmd/root.go
@@ -6,19 +6,19 @@ import (
   "github.com/dgutierrez1287/local-kube/logger"
   "github.com/dgutierrez1287/local-kube/util"
 
-	"github.com/hashicorp/go-hclog"
 	"github.com/spf13/cobra"
 )
 
 var debug bool
 var logColorize bool
+var clusterName string
 
 var RootCmd = &cobra.Command{
   Use: "local-kube",
   Short: "A program to create and manage a local kube cluster using vagrant",
   Long: "A program to create and manage a local kube cluster using vagrant",
   PersistentPreRun: func(cmd *cobra.Command, args []string) {
-    setupLogging()
+    logger.InitLogging(debug, logColorize)
   }, 
   Run: func(cmd *cobra.Command, args []string) {
     fmt.Println(util.TitleText)
@@ -31,6 +31,9 @@ func Execute() error {
 }
 
 func init() {
+  // cluster name
+  RootCmd.PersistentFlags().StringVarP(&clusterName, "cluster", "c", "", "The cluster to run the action on")
+
   // debugging flag
   RootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "Enable debug output")
 
@@ -38,33 +41,4 @@ func init() {
   RootCmd.PersistentFlags().BoolVarP(&logColorize, "colorize", "", true, "Enable/Disable output colorization")
 }
 
-func setupLogging() {
-  // set up logger 
 
-  var colorOpt hclog.ColorOption
-  
-  // debug output setup
-  if debug {
-    logger.LogLevel = "DEBUG"
-    fmt.Println("Debugging enabled")
-  } else {
-    logger.LogLevel = "INFO"
-  }
-
-  // colorization setup
-  if logColorize {
-    colorOpt = hclog.ColorOption(hclog.AutoColor)
-  } else {
-    fmt.Println("Output colorization disabled")
-    colorOpt = hclog.ColorOption(hclog.ColorOff)
-  }
-
-  fmt.Printf("loglevel %s \n", logger.LogLevel)
-
-  // create global logger
-  logger.Logger = hclog.New(&hclog.LoggerOptions{
-    Name: "local-kube",
-    Level: hclog.LevelFromString(logger.LogLevel),
-    Color: colorOpt,
-  })
-}
