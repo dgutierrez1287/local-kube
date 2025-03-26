da9642df5331796564b3b203c3954a9061d92e63
Diego Gutierrez <dgutierrez1287@gmail.com>
2025-03-25 20:50:53 -0400

    added vagrant client wrapper to allow for easier mocking

---
+++ cluster/vagrantClientFactory.go
@@ -0,0 +1,37 @@
+package cluster
+
+import (
+  vagrant "github.com/bmatcuk/go-vagrant"
+)
+
+type VagrantClientInterface interface {
+  Status() *vagrant.StatusCommand
+  Up() *vagrant.UpCommand
+  Destroy() *vagrant.DestroyCommand
+}
+
+//type VagrantClientFactory func(vagrantDirPath string) (VagrantClientInterface, error)
+
+type DefaultVagrantClient struct {
+  client *vagrant.VagrantClient
+}
+
+func (v *DefaultVagrantClient) Status() *vagrant.StatusCommand {
+  return v.client.Status()
+}
+
+func (v *DefaultVagrantClient) Up() *vagrant.UpCommand {
+  return v.client.Up()
+}
+
+func (v *DefaultVagrantClient) Destroy() *vagrant.DestroyCommand {
+  return v.client.Destroy()
+}
+
+func NewVagrantClient(vagrantDirPath string) (VagrantClientInterface, error) {
+  client, err := vagrant.NewVagrantClient(vagrantDirPath)
+  if err != nil {
+    return nil, err
+  }
+  return &DefaultVagrantClient{client: client}, nil
+}
---
+++ cluster/vagrantClientMocks.go
@@ -0,0 +1,37 @@
+package cluster
+
+import (
+	"context"
+
+	vagrant "github.com/bmatcuk/go-vagrant"
+)
+
+type MockVagrantClient struct {
+  mockStatus map[string]string 
+}
+
+var _ VagrantClientInterface = (*MockVagrantClient)(nil)
+
+func NewMockVagrantClientStatus(status map[string]string) *MockVagrantClient {
+  return &MockVagrantClient{mockStatus: status}
+}
+
+func (m *MockVagrantClient) Status() *vagrant.StatusCommand {
+  return &vagrant.StatusCommand{
+    BaseCommand: vagrant.BaseCommand{
+      Context: context.Background(),
+    },
+    StatusResponse: vagrant.StatusResponse{
+      Status: m.mockStatus,
+    },
+    
+  }
+}
+
+func (m *MockVagrantClient) Up() *vagrant.UpCommand {
+  return &vagrant.UpCommand{}
+}
+
+func (m *MockVagrantClient) Destroy() *vagrant.DestroyCommand {
+  return &vagrant.DestroyCommand{}
+}
