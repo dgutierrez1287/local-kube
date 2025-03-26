a03c0a6fb62ce062d0180893c0d61376d6cb4d7a
Diego Gutierrez <dgutierrez1287@gmail.com>
2025-03-25 20:47:43 -0400

    updated checks to check for existing cluster and added a test, more tests will be needed but vagrantfile and other things need to get further

--- cluster/checks.go
+++ cluster/checks.go
@@ -1,12 +1,9 @@
 package cluster
 
 import (
-	"os"
-	"path/filepath"
+	"errors"
 
-	vagrant "github.com/bmatcuk/go-vagrant"
 	"github.com/dgutierrez1287/local-kube/logger"
-	"github.com/dgutierrez1287/local-kube/settings"
 )
 
 /* This is going to be used by a few different
@@ -25,59 +22,61 @@ exists for the cluster
 created - if the cluster has any machines from it that are
 in any state
 */
-func CheckForExistingCluster(clusterName string) (bool, string){
+func CheckForExistingCluster(appDir string, clusterName string, 
+client VagrantClientInterface) (bool, string, error){
   var dirExists bool
 
   logger.Logger.Debug("checking if any cluster currently exists")
 
-  appDir := settings.GetAppDirPath()
-  clusterDirPath := filepath.Join(appDir, clusterName)
-
-  dirExists = ClusterDirExists(clusterName)
+  dirExists, err := ClusterDirExists(appDir, clusterName)
+  if err != nil {
+    logger.Logger.Error("Error checking for cluster dir", "cluster", clusterName)
+    return false, "", err
+  }
   
   // if the cluster dir doesnt exist stopping looking 
   //now and return
   if !dirExists {
     logger.Logger.Debug("cluster directory doesnt exist there is no cluster present")
-    return false, ""
+    return false, "", nil
   }
 
   logger.Logger.Debug("checking for current cluster that has been created")
 
-  // get a vagrant client for that cluster
-  client, err := vagrant.NewVagrantClient(clusterDirPath)
-  if err != nil {
-    logger.Logger.Error("Error getting vagrant client", "error", err)
-    os.Exit(400)
-  }
-
   statusCmd := client.Status()
+
+  logger.Logger.Debug("statusCmd", statusCmd)
+  
+  if statusCmd == nil {
+    logger.Logger.Error("Error status command is nil")
+    return false, "", errors.New("status command is nil")
+  }
   
   if err := statusCmd.Start(); err != nil {
     logger.Logger.Error("Error running the vagrant status command", "error", err)
-    os.Exit(400)
+    return false, "", err
   }
 
   if err := statusCmd.Wait(); err != nil {
     logger.Logger.Error("Error waiting for the status command to return", "error", err)
-    os.Exit(400)
+    return false, "", err
   }
 
   resp := statusCmd.StatusResponse
   respErrors := resp.ErrorResponse
 
   if respErrors.Error != nil {
-    logger.Logger.Error("Error getting the vagrant status", "error", statusCmd.Error)
-    os.Exit(400)
+    logger.Logger.Error("Error getting the vagrant status", "error", respErrors.Error)
+    return false, "", respErrors.Error
   }
 
   statuses := resp.Status
 
   if len(statuses) == 0 {
     logger.Logger.Debug("Directory for cluster exists but no vms for cluster are present")
-    return true, "directory"
+    return true, "directory", nil
   }
 
   logger.Logger.Debug("Directory for cluster and vms are present, punt this back to the user")
-  return true, "created"
+  return true, "created", nil
 }
---
+++ cluster/checks_test.go
@@ -0,0 +1,72 @@
+package cluster
+
+import (
+	"testing"
+
+	"github.com/dgutierrez1287/local-kube/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckForExistingClusterNoDir(t *testing.T) {
+  mockStatus := map[string]string{}
+  clusterName := "test-cluster"
+
+  err := util.MockAppDirSetup()
+  assert.NoError(t, err)
+
+  defer util.MockAppDirCleanup()
+
+  vagrantClientMock := NewMockVagrantClientStatus(mockStatus)
+
+  status, state, err := CheckForExistingCluster(util.MockAppDir, clusterName, vagrantClientMock)
+  
+  assert.False(t, status)
+  assert.Equal(t, state, "")
+  assert.NoError(t, err)
+}
+
+// func TestCheckForExistingClusterDirOnly(t *testing.T) {
+//   mockStatus := map[string]string{}
+//   clusterName := "test-cluster"
+//
+//   err := util.MockAppDirSetup()
+//   assert.NoError(t, err)
+//
+//   err = os.Mkdir(filepath.Join(util.MockAppDir, clusterName), 0755)
+//   assert.NoError(t, err)
+//
+//   defer util.MockAppDirCleanup()
+//
+//   vagrantClientMock := NewMockVagrantClientStatus(mockStatus)
+//
+//   status, state, err := CheckForExistingCluster(util.MockAppDir, clusterName, vagrantClientMock)
+//
+//   assert.True(t, status)
+//   assert.Equal(t, state, "directory")
+//   assert.NoError(t, err)
+// }
+
+// func TestCheckForExistingClusterWithVms(t *testing.T) {
+//   mockStatus := map[string]string{
+//     "vm-1": "running",
+//     "vm-2": "running",
+//   }
+//   clusterName := "test-cluster"
+//
+//   err := util.MockAppDirSetup()
+//   assert.NoError(t, err)
+//
+//   defer util.MockAppDirCleanup()
+//
+//   err = os.Mkdir(filepath.Join(util.MockAppDir, clusterName), 0755)
+//   assert.NoError(t, err)
+//
+//   vagrantClientMock := NewMockVagrantClientStatus(mockStatus)
+//  
+//   status, state, err := CheckForExistingCluster(util.MockAppDir, clusterName, vagrantClientMock)
+//
+//   assert.True(t, status)
+//   assert.Equal(t, state, "created")
+//   assert.NoError(t, err)
+// }
+
