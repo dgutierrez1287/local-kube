926b83fec45c1a4828c4c44a63107b2b30a9d25d
Diego Gutierrez <dgutierrez1287@gmail.com>
2025-03-27 19:03:04 -0400

    added helper function to read file to string array to help with unit test verification

--- util/test_helpers.go
+++ util/test_helpers.go
@@ -2,7 +2,13 @@ package util
 
 import (
   "os"
+  "bufio"
 )
+
+/*
+  Helper functions for testing, these will be
+  used in many places
+*/
 
 var MockAppDir = "./mock/appdir"
 var MockAnsibleRoleDir = "./mock/appdir/ansible-roles"
@@ -28,3 +34,22 @@ func MockAppDirCleanup() error {
   return nil 
 }
 
+func ReadFileToStringArray(path string) ([]string, error) {
+  var fileLines []string
+
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    fileLines = append(fileLines, scanner.Text())
+  }
+
+  if err := scanner.Err(); err != nil {
+    return nil, err
+  }
+  return fileLines, nil
+}
