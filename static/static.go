68ce6d7a48e1281a2e622cc37ba4b945f2f3c590
Diego Gutierrez <dgutierrez1287@gmail.com>
2025-04-02 20:46:44 -0400

    added functions to list all files in a static embedded directory and added tests

--- static/static.go
+++ static/static.go
@@ -2,23 +2,78 @@ package static
 
 import (
 	"embed"
+	"io/fs"
 
 	"github.com/dgutierrez1287/local-kube/logger"
 )
 
 //embed directory for remote scripts
 //(scripts that run on the master node to provision
-//go:embed remote_scripts
-var rsScripts embed.FS
+//go:embed remote_scripts/*
+var remoteScriptsFs embed.FS
 
 //embed directory for provision scripts
 //scripts that are present on every node that 
 //are used for provision tasks during vagrant up
-var psScripts embed.FS
+//go:embed provision_scripts/*
+var provisionScriptsFs embed.FS
 
+func GetProvisionScriptsFS() fs.FS {
+  return provisionScriptsFs
+}
 
-func ReadProvisionScriptFile(path string) (string, error) {
-  data, err := psScripts.ReadFile(path)
+func GetRemoteScriptsFS() fs.FS {
+  return remoteScriptsFs
+}
+
+/*
+  This will return an array of all the files in the 
+  provision scripts embeded directory
+*/
+func ListProvisonScripts() ([]string, error) {
+  fileNames := []string{}
+
+  files, err := fs.ReadDir(provisionScriptsFs, "provision_scripts")
+  if err != nil {
+    logger.Logger.Error("Error reading provision_scripts directory")
+    return fileNames, err
+  }
+
+  for _, file := range files {
+    logger.Logger.Debug("Adding script file to list", "file", file.Name())
+    fileNames = append(fileNames, file.Name())
+  }
+  return fileNames, nil
+}
+
+/*
+  This will return an array of all the file in the
+  remote scripts embeded directory
+*/
+func ListRemoteScripts() ([]string, error) {
+  fileNames := []string{}
+
+  files, err := fs.ReadDir(remoteScriptsFs, "remote_scripts")
+  if err != nil {
+    logger.Logger.Error("Error reading remote_scripts directory")
+    return fileNames, err
+  }
+
+  for _, file := range files {
+    logger.Logger.Debug("Adding script file to list", "file", file.Name())
+    fileNames = append(fileNames, file.Name())
+  }
+  return fileNames, nil
+}
+
+/*
+  This will return a string of the provision script 
+  file content
+*/
+func ReadProvisionScriptFile(scriptName string) (string, error) {
+  path := "provision_scripts/" + scriptName
+
+  data, err := provisionScriptsFs.ReadFile(path)
   if err != nil {
     logger.Logger.Error("Error reading static provision script", "path", path)
     return "", err
@@ -26,8 +81,14 @@ func ReadProvisionScriptFile(path string) (string, error) {
   return string(data), nil
 }
 
-func ReadRemoteScriptFile(path string) (string, error){
-  data, err := rsScripts.ReadFile(path)
+/*
+  This will return a string of the remote script 
+  file content
+*/
+func ReadRemoteScriptFile(scriptName string) (string, error){
+  path := "remote_scripts/" + scriptName
+
+  data, err := remoteScriptsFs.ReadFile(path)
   if err != nil {
     logger.Logger.Error("Error reading static remote script", "path", path)
     return "", err
---
+++ static/static_test.go
@@ -0,0 +1,74 @@
+package static
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/dgutierrez1287/local-kube/logger"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestMain is executed before running any tests
+func TestMain(m *testing.M) {
+	// Initialize the logger before running any tests
+	logger.InitLogging(false, true)
+	os.Exit(m.Run())
+}
+
+func TestListProvisionScripts(t *testing.T) {
+  fileNames, err := ListProvisonScripts()
+
+  assert.NoError(t, err)
+
+  // verify file names in the list
+  assert.Contains(t, fileNames, "install-yq.sh")
+  assert.Contains(t, fileNames, "disk-expand.sh")
+  assert.Contains(t, fileNames, "setup-hostsfile.sh")
+  assert.Contains(t, fileNames, "multi-node-provision.sh")
+}
+
+func TestListRemoteScripts(t *testing.T) {
+  fileNames, err := ListRemoteScripts()
+
+  assert.NoError(t, err)
+
+  // verify file names in the list
+  assert.Contains(t, fileNames, "ha-provision.sh")
+  assert.Contains(t, fileNames, "single-provision.sh")
+  assert.Contains(t, fileNames, "correct_kubeconfig.py")
+}
+
+func TestReadProvisionScriptFile(t *testing.T) {
+  fileContent, err := ReadProvisionScriptFile("disk-expand.sh")
+
+  assert.NoError(t, err)
+  
+  // verify first line is the shebang
+  firstLine := strings.Split(fileContent, "\n")[0]
+  assert.Equal(t, firstLine, "#!/usr/bin/env bash")
+}
+
+func TestReadProvisionScriptError(t *testing.T) {
+  _, err := ReadProvisionScriptFile("does-not-exist.sh")
+
+  // should return an error
+  assert.Error(t, err)
+}
+
+func TestReadRemoteScriptFile(t *testing.T) {
+  fileContent, err := ReadRemoteScriptFile("single-provision.sh")
+
+  assert.NoError(t, err)
+
+  // verify first line is the shebang
+  firstLine := strings.Split(fileContent, "\n")[0]
+  assert.Equal(t, firstLine, "#!/usr/bin/env bash")
+}
+
+func TestReadRemoteScriptError(t *testing.T) {
+  _, err := ReadRemoteScriptFile("does-not-exist.sh")
+
+  //should be an error 
+  assert.Error(t, err)
+}
