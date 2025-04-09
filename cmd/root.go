a8f5d88f8787ecff17cff1e598b92b05b869c433
Diego Gutierrez <dgutierrez1287@gmail.com>
2025-04-08 20:15:27 -0400

    added machineOutput flag to set if output should only be one json output that can be read by a machine

--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,14 @@ import (
 var debug bool
 var logColorize bool
 var clusterName string
+var machineOutput bool
 
 var RootCmd = &cobra.Command{
   Use: "local-kube",
   Short: "A program to create and manage a local kube cluster using vagrant",
   Long: "A program to create and manage a local kube cluster using vagrant",
   PersistentPreRun: func(cmd *cobra.Command, args []string) {
-    logger.InitLogging(debug, logColorize)
+    logger.InitLogging(debug, logColorize, machineOutput)
   }, 
   Run: func(cmd *cobra.Command, args []string) {
     fmt.Println(util.TitleText)
@@ -39,6 +40,11 @@ func init() {
 
   // output colorization flag
   RootCmd.PersistentFlags().BoolVarP(&logColorize, "colorize", "", true, "Enable/Disable output colorization")
+
+  // machine only output flag
+  // This will suppess all other output and only output json that 
+  // is machine readable
+  RootCmd.PersistentFlags().BoolVarP(&machineOutput, "machine-output", "m", false, "Enables machine only output, json that can be used by executing script")
 }
 
 
