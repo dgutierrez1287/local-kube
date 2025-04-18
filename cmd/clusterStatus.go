7dc9528cbbbd580b3cb17c9693652ba1fd34678c
Diego Gutierrez <dgutierrez1287@gmail.com>
2025-04-18 04:57:01 -0400

    updated function calls and added some logging

--- cmd/clusterStatus.go
+++ cmd/clusterStatus.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/dgutierrez1287/local-kube/cluster"
 	"github.com/dgutierrez1287/local-kube/logger"
@@ -20,8 +19,6 @@ var clusterStatusCmd = &cobra.Command{
   Run: func(cmd *cobra.Command, args []string) {
     var machineReadableOutput output.MachineOutput
 
-    logger.Logger.Debug("machineOutput flag is", "value", machineOutput)
-
     if machineOutput && debug {
       logger.Logger.Error("Error you can't have machine output set and debug set")
       os.Exit(20)
@@ -31,41 +28,14 @@ var clusterStatusCmd = &cobra.Command{
       fmt.Println(util.TitleText)
     }
 
+    logger.Logger.Debug("machineOutput flag is", "value", machineOutput)
+
     appDir := settings.GetAppDirPath()
-    clusterDir := filepath.Join(appDir, clusterName)
-
-    if !machineOutput {
-      logger.Logger.Info("Getting Vagrant cluster for cluster", "name", clusterName, "dir", clusterDir)
-    }
-
-    // get vagrant client and check for error 
-    vagrantClient, err := cluster.NewVagrantClient(clusterDir)
-    if err != nil {
-      if !machineOutput {
-        logger.Logger.Error("Error getting vagrant client for cluster", "name", clusterName, "error", err)
-        os.Exit(100)
-      } else {
-        machineReadableOutput.ExitCode = 100
-        machineReadableOutput.ErrorMessage = fmt.Sprintf("Error getting vagrant client %v", err)
-        output, eCode := machineReadableOutput.GetMachineOutputJson()
-        fmt.Println(output)
-        os.Exit(eCode)
-      }
-    }
     
     // Run an initial check if the cluster directory exists and see if any machines are present
-    created, createdStatus, err := cluster.CheckForExistingCluster(appDir, clusterName, vagrantClient, machineOutput)
+    created, createdStatus, err := cluster.CheckForExistingCluster(appDir, clusterName, machineOutput)
     if err != nil {
-      if !machineOutput {
-        logger.Logger.Error("Error checking for the initial cluster status", "error", err)
-        os.Exit(110)
-      } else {
-        machineReadableOutput.ExitCode = 110
-        machineReadableOutput.ErrorMessage = fmt.Sprintf("Error checking inital cluster status %v", err)
-        output, eCode := machineReadableOutput.GetMachineOutputJson()
-        fmt.Println(output)
-        os.Exit(eCode)
-      }
+      logger.LogErrorExit("Error checking for the inital cluster status", 110, err)
     }
 
     // Just the directory exists but no machines are present
@@ -85,23 +55,12 @@ var clusterStatusCmd = &cobra.Command{
 
     // machines are present, call to get a more detailed status
     if created && createdStatus == "created" {
-      if !machineOutput {
-        logger.Logger.Info("Getting detailed cluster and machine status")
-      }
+      logger.LogInfo("Getting detailed cluster and machine status")
 
       // Get detailed cluster and machine status for output
-      clusterStatus, statuses, err := cluster.GetDetailedClusterStatus(appDir, clusterName, vagrantClient, machineOutput)
+      clusterStatus, statuses, err := cluster.GetDetailedClusterStatus(appDir, clusterName, machineOutput)
       if err != nil {
-        if !machineOutput {
-          logger.Logger.Error("Error getting detailed cluster status", "error", err)
-          os.Exit(110)
-        } else {
-          machineReadableOutput.ExitCode = 110
-          machineReadableOutput.ErrorMessage = fmt.Sprintf("Error getting detailed machine status %v", err)
-          output, eCode := machineReadableOutput.GetMachineOutputJson()
-          fmt.Println(output)
-          os.Exit(eCode)
-        }
+        logger.LogErrorExit("Error getting detailed cluster status", 110, err)
       }
 
       // output status in the desired format 
